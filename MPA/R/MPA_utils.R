f435a45f0c7b83b944daef5e9af37f2e0b3f2c01
Cristina Mitrea <mitrea@umich.edu>
2023-03-05 23:59:31 -0500

    Updated code, tests and documentation for compute reaction and pathway score and hello functions

--- MPA/R/MPA_utils.R
+++ MPA/R/MPA_utils.R
@@ -1,7 +1,13 @@
 #' @import dplyr
 library(dplyr)
+#' @import utils
+library(utils)
 
-# read expression data
+#' Retrieves from a tab-separated file a data.frame with the metabolite data
+#' @param file_name the metabolite level data file name - tab-separated file with
+#' metabolite average values for condition and control
+#' rows are metabolites, rownames are metabolites ids and 2 columns condition and control
+#' @return data.frame with the expression data
 #' @export
 read_expression_data <- function(file_name){
   expr_data <- read.table(file = file_name, header = T, sep = "\t")
@@ -12,14 +18,16 @@ read_expression_data <- function(file_name){
 # metab cond  ctrl
 # m1  10  11
 # m2  12  20
-file_name = "test_expr_file.txt"
-df = data.frame(cond = c(10,12), ctrl = c(11,20), row.names = c("m1","m2"))
-write.table(df, file_name, sep = "\t")
-df1 = read_expression_data(file_name)
-print(df1)
-print(df1==df)
+# file_name = "test_expr_file.txt"
+# df = data.frame(cond = c(10,12), ctrl = c(11,20), row.names = c("m1","m2"))
+# write.table(df, file_name, sep = "\t")
+# df1 = read_expression_data(file_name)
+# print(df1)
+# print(df1==df)
 
-# read pathway data
+#' Reads pathway data from file
+#' @param file_name of the metabolic pathway in SBML format
+#' @return list of reactions
 #' @export
 read_pathway_data <- function(file_name){
   pathway_data <- NULL
@@ -28,20 +36,40 @@ read_pathway_data <- function(file_name){
 }
 
 
+#' Computes a perturbation score - a metric that evaluates reaction disruption
+#' based on the input metabolite data
+#' @param expr_data the metabolite level data - data frame with metabolite average values for condition and control
+#' rows are metabolites, rownames are metabolites ids and 2 columns condition and control
+#' @param reaction_data the reaction data in a list format list
+#' left and right with each list having another list of elements which
+#' are the metabolites and its coefficient
+#' @return numeric value the reaction perturbation score
+#' @export
+compute_reaction_score <- function(expr_data, reaction_data){
+  reaction_score <- 0
+  common_metabolites <- intersect(rownames(expr_data), rownames(reaction_data$right))
+  reaction_score <- min(abs(log2(as.numeric(expr_data[common_metabolites,1])/as.numeric(expr_data[common_metabolites,2]))))
+  # compute perturbation score using expression data and pathway data
+  return(reaction_score)
+}
+
 #' Computes a perturbation score - a metric that evaluates pathway disruption
 #' based on the input metabolite data
 #' @param expr_data the metabolite level data - data frame with metabolite average values for condition and control
-#' rows are metabolites, rownames are metabolites idsand 2 columns condition and control
+#' rows are metabolites, rownames are metabolites ids and 2 columns condition and control
 #' @param pathway_data the pathway data in a SBML format - this will be a custom structure
 #' @return numeric value the pathway perturbation score
 #' @export
 compute_pathway_score <- function(expr_data, pathway_data){
   pert_score <- 0
-  pert_score <- expr_data + pathway_data
+  # pert_score <- expr_data + pathway_data
   # compute perturbation score using expression data and pathway data
   return(pert_score)
 }
 
+#' Retrieves the files for the reactome files from url
+#' @param reactome_url the reactome pathway data url
+#' @return named vector with the pathway file names
 #' @export
 get_pathway_files <- function(reactome_url){
   pathways_file_names <- NULL
@@ -49,6 +77,13 @@ get_pathway_files <- function(reactome_url){
   return(pathways_file_names)
 }
 
+#' Performs metabolic pathway analysis
+#' based on the input metabolite data and the input pathways
+#' @param expr_data_file the file with the metabolite level data - tab separated file with
+#' metabolite average values for condition and control
+#' rows are metabolites, rownames are metabolites ids and 2 columns condition and control
+#' @param reactome_url the reactome pathway data url
+#' @return data.frame with the ranked pathway and pathway scores and p-values
 #' @export
 MPA_analysis <- function(expr_data_file, reactome_url){
   expr_data <- read_expression_data(expr_data_file)
@@ -57,11 +92,13 @@ MPA_analysis <- function(expr_data_file, reactome_url){
   pathway_pvals <- NULL
   # compute pathway scores and p-values
   # go through the list of pathway files
-  for (file_name in pathways_file_names)
+  for (file_name in pathways_file_names){
+
     # and call the read_pathway_data
     pathway_data <- read_pathway_data(file_name)
-  parhway_score <- compute_pathway_score(expr_data, pathway_data)
-  pathway_scores <- c(pathway_scores, pathway_score)
+    pathway_score <- compute_pathway_score(expr_data, pathway_data)
+    pathway_scores <- c(pathway_scores, pathway_score)
+  }
   return(data.frame(pathway_scores, pathway_pvals))
 }
 
@@ -87,17 +124,26 @@ MPA_analysis <- function(expr_data_file, reactome_url){
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+#' Displays hello, world and the parameter x
+#' based on the input metabolite data
+#' @param x the text to display next to Hello, world!
+#' @return string x the text that was displaied next to Hello, world!
 #' @export
 hello <- function(x) {
   print(paste("Hello, world!", x))
+  return(x)
 }
 
+#' Displays Hello again, world!
+#' based on the input metabolite data
+#' @return string Hello again, world!
 #' @export
 hello2 <- function() {
-  print("Hello again, world!")
+  text <- "Hello again, world!"
+  print(text)
 }
 
 
-hello("BIOINF576")
-df <- data.frame(x = c(1,2), y = c(3,4), z = c(5,6))
-dplyr::select(df, z)
+# hello("BIOINF576")
+# df <- data.frame(x = c(1,2), y = c(3,4), z = c(5,6))
+# dplyr::select(df, z)
--- MPA/tests/testthat/test-utils.R
+++ MPA/tests/testthat/test-utils.R
@@ -4,9 +4,28 @@ test_that("multiplication works", {
 
 
 test_that("compute_pathway_score", {
-  expr_data <- 2
-  pathway_data <- 1
-  input3 <- 7
-  expected_score <- 2
-  expect_equal(compute_pathway_score(expr_data, pathway_data, input3), expected_score)
+  expr_data <- data.frame(row.names = c("m1","m2","m3"),
+                          condition = c(1.4,2,3),
+                          control = c(3,4,5))
+  reaction_data <- list(left = data.frame(row.names = c("m155","m20","m30"),
+                                          coefficient = c(1,1,1)),
+                        right = data.frame(row.names = c("m11","m12","m13"),
+                                           coefficient = c(1,1,1)))
+  pathway_data <- list(reaction1 = reaction_data,
+                       reaction2 = reaction_data)
+  expected_score <- 0
+  expect_equal(compute_pathway_score(expr_data, pathway_data), expected_score)
+})
+
+
+test_that("compute_reaction_score", {
+  expr_data <- data.frame(row.names = c("m1","m2","m3"),
+                          condition = c(1.4,2,3),
+                          control = c(3,4,5))
+  reaction_data <- list(left = data.frame(row.names = c("m155","m20","m30"),
+                                         coefficient = c(1,1,1)),
+                       right = data.frame(row.names = c("m1","m2","m13"),
+                                          coefficient = c(1,1,1)))
+  expected_score <- 1
+  expect_equal(compute_reaction_score(expr_data, reaction_data), expected_score)
 })
